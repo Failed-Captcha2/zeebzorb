0e1335380ab25f9432530b6fc337f95f4c140951
Failed-Captcha <crowe.elshobary@gmail.com>
2025-07-29 00:45:59 -0400

    added pen movement
    -player can switch between controlling pen and player by holding shift
    -player can toggle drawing on/off with E and backslash keys respectively

--- zeebzorb/Assets/Scripts/playerController.cs
+++ zeebzorb/Assets/Scripts/playerController.cs
@@ -2,25 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class playerController : MonoBehaviour
 {
     public bool player1;
     public float speed;
-    private Rigidbody2D rb2D;
+    public GameObject pen;
+    public Color penColor;
+    public GameObject lineObj;
+    private Rigidbody2D penRB;
+    private Transform penTransform;
+    private SpriteRenderer penSprite;
+    Line lineScript;
+    private Rigidbody2D playerRB;
+    private Vector3 move;
     private KeyCode Up;
     private KeyCode Down;
     private KeyCode Left;
     private KeyCode Right;
+    private KeyCode MovePen;
+    private KeyCode Draw;
+    private bool controlPen;
+    private bool drawing;
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        playerRB = GetComponent<Rigidbody2D>();
         if (player1)
         {
             Up = KeyCode.W;
             Down = KeyCode.S;
             Left = KeyCode.A;
             Right = KeyCode.D;
+            Draw = KeyCode.E;
+            MovePen = KeyCode.LeftShift;
         }
         else
         {
@@ -28,29 +43,82 @@ public class playerController : MonoBehaviour
             Down = KeyCode.DownArrow;
             Left = KeyCode.LeftArrow;
             Right = KeyCode.RightArrow;
+            Draw = KeyCode.Backslash;
+            MovePen = KeyCode.RightShift;
         }
+
+        penRB = pen.GetComponent<Rigidbody2D>();
+        penTransform = pen.GetComponent<Transform>();
+        penSprite = pen.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb2D.velocity = Vector3.zero;
+
+        playerRB.velocity = Vector3.zero;
+        penRB.velocity = Vector3.zero;
+        move = Vector3.zero;
+
         if (Input.GetKey(Up))
         {
-            rb2D.velocity = new Vector3(rb2D.velocity.x, speed, 0);
+            move = new Vector3(move.x, speed, 0);
         }
         if (Input.GetKey(Down))
         {
-            rb2D.velocity = new Vector3(rb2D.velocity.x, -1*speed, 0);
+            move = new Vector3(move.x, -1 * speed, 0);
         }
         if (Input.GetKey(Left))
         {
-            rb2D.velocity = new Vector3(-1*speed,rb2D.velocity.y, 0);
+            move = new Vector3(-1 * speed, move.y, 0);
         }
         if (Input.GetKey(Right))
         {
-            rb2D.velocity = new Vector3(speed,rb2D.velocity.y, 0);
+            move = new Vector3(speed, move.y, 0);
         }
+
+        if (Input.GetKeyDown(Draw) && controlPen && !drawing)
+        {
+            drawing = true;
+            GameObject newLine = Instantiate(lineObj);
+            lineScript = newLine.GetComponent<Line>();
+            newLine.GetComponent<LineRenderer>().startColor = penSprite.color;
+            newLine.GetComponent<LineRenderer>().endColor = penSprite.color;
+            
+        }
+        else if (drawing && (Input.GetKeyDown(Draw)|| !controlPen))
+        {
+            drawing = false;
+            lineScript = null;
+        }
+
+        if (Input.GetKeyUp(MovePen))
+        {
+            if (controlPen)
+            {
+                controlPen = false;
+            }
+            else
+            {
+                controlPen = true;
+            }
+        }
+
+        if (controlPen)
+        {
+            penRB.velocity = move;
+        }
+        else
+        {
+            playerRB.velocity = move;
+        }
+
+        if (lineScript != null)
+        {
+            lineScript.UpdateLine(penTransform.position);
+        }
+
+
 
     }
 
